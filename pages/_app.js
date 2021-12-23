4861a4fd11c3ad66536b81fdf0bb2739261388dd
Flavio Coutinho <coutinhoflavio20@gmail.com>
2021-12-23 21:48:30 +0100

    add blog-starter

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/reset.css'
+import '../styles/index.css'
+import '../styles/main.css'
+
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
